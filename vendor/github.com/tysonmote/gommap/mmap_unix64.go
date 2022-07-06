c6c0b78ee3453504e5d30b03ca1cb0506156ae68
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-07-06 01:00:37 +0000

    updated

---
+++ vendor/github.com/tysonmote/gommap/mmap_unix64.go
@@ -0,0 +1,11 @@
+// +build linux freebsd
+// +build amd64 arm64
+
+package gommap
+
+import "syscall"
+
+func mmap_syscall(addr, length, prot, flags, fd uintptr, offset int64) (uintptr, error) {
+	addr, _, err := syscall.Syscall6(syscall.SYS_MMAP, addr, length, prot, flags, fd, uintptr(offset))
+	return addr, err
+}
