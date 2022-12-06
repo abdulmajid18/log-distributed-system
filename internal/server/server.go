d5da1640f7a735ff9ed1021fc7b7ccd99239ae7c
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-06 21:36:00 +0000

    updated: server

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,30 +2,27 @@ package server
 
 import (
 	"context"
-	"time"
 
 	api "github.com/abdulmajid18/log-distributed-system/api/v1"
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
-
-	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	"go.opencensus.io/plugin/ocgrpc"
-	"go.opencensus.io/stats/view"
-	"go.opencensus.io/trace"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/peer"
-	"google.golang.org/grpc/status"
+	// grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	// grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	// grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	// grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	// "go.opencensus.io/plugin/ocgrpc"
+	// "go.opencensus.io/stats/view"
+	// "go.opencensus.io/trace"
+	// "go.uber.org/zap"
+	// "go.uber.org/zap/zapcore"
+	// "google.golang.org/grpc/codes"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/peer"
+	// "google.golang.org/grpc/status"
 )
 
-type Authorizer interface {
-	Authorize(subject, object, action string) error
-}
+// type Authorizer interface {
+// 	Authorize(subject, object, action string) error
+// }
 
 type CommitLog interface {
 	Append(*api.Record) (uint64, error)
@@ -33,17 +30,17 @@ type CommitLog interface {
 }
 
 type Config struct {
-	CommitLog  CommitLog
-	Authorizer Authorizer
+	CommitLog CommitLog
+	// Authorizer Authorizer
 }
 
-const (
-	objectWildcard = "*"
-	produceAction  = "produce"
-	consumeAction  = "consume"
-)
+// const (
+// 	objectWildcard = "*"
+// 	produceAction  = "produce"
+// 	consumeAction  = "consume"
+// )
 
-var _ api.LogServer = (*grpcServer)(nil)
+// var _ api.LogServer = (*grpcServer)(nil)
 
 type grpcServer struct {
 	api.UnimplementedLogServer
@@ -57,60 +54,8 @@ func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 	return srv, nil
 }
 
-// func NewGRPCServer(config *Config) (*grpc.Server, error) {
-// 	gsrv := grpc.NewServer()
-// 	srv, err := newgrpcServer(config)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	api.RegisterLogServer(gsrv, srv)
-// 	return gsrv, nil
-// }
-
-func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (
-	*grpc.Server,
-	error,
-) {
-	logger := zap.L().Named("Server")
-	zapOpts := []grpc_zap.Option{
-		grpc_zap.WithDurationField(
-			func(duration time.Duration) zapcore.Field {
-				return zap.Int64(
-					"grpc.time_ns",
-					duration.Nanoseconds())
-			},
-		),
-	}
-	// Hen we wantto customize the sampling of traceing to a probability of 50%
-	// not to miss import tracing
-	// halfSampler := trace.ProbabilitySampler(0.5)
-	// trace.ApplyConfig(trace.Config{
-	// 	DefaultSampler: func(p trace.SamplingParameters) trace.SamplingDecision {
-	// 		if strings.Contains(p.Name, "Produce") {
-	// 			return trace.SamplingDecision{Sample: true}
-	// 		}
-	// 		return halfSampler(p)
-	// 	},
-	// })
-	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-	err := view.Register(ocgrpc.DefaultServerViews...)
-	if err != nil {
-		return nil, err
-	}
-
-	opts = append(opts, grpc.StreamInterceptor(
-		grpc_middleware.ChainStreamServer(
-			grpc_auth.StreamServerInterceptor(authenticate),
-			grpc_zap.StreamServerInterceptor(logger, zapOpts...),
-		)), grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-		grpc_ctxtags.UnaryServerInterceptor(),
-		grpc_zap.UnaryServerInterceptor(logger, zapOpts...),
-		grpc_auth.UnaryServerInterceptor(authenticate),
-	)),
-		grpc.StatsHandler(&ocgrpc.ServerHandler{}),
-	)
-
-	gsrv := grpc.NewServer(opts...)
+func NewGRPCServer(config *Config) (*grpc.Server, error) {
+	gsrv := grpc.NewServer()
 	srv, err := newgrpcServer(config)
 	if err != nil {
 		return nil, err
@@ -120,29 +65,14 @@ func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (
 }
 
 func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
-	if err := s.Authorizer.Authorize(
-		subject(ctx),
-		objectWildcard,
-		produceAction,
-	); err != nil {
-		return nil, err
-	}
 	offset, err := s.CommitLog.Append(req.Record)
 	if err != nil {
 		return nil, err
 	}
-
 	return &api.ProduceResponse{Offset: offset}, nil
 }
 
 func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
-	if err := s.Authorizer.Authorize(
-		subject(ctx),
-		objectWildcard,
-		produceAction,
-	); err != nil {
-		return nil, err
-	}
 	record, err := s.CommitLog.Read(req.Offset)
 	if err != nil {
 		return nil, err
@@ -188,28 +118,149 @@ func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_Consu
 	}
 }
 
-type subjectContextKey struct{}
+// func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (
+// 	*grpc.Server,
+// 	error,
+// ) {
+// 	logger := zap.L().Named("Server")
+// 	zapOpts := []grpc_zap.Option{
+// 		grpc_zap.WithDurationField(
+// 			func(duration time.Duration) zapcore.Field {
+// 				return zap.Int64(
+// 					"grpc.time_ns",
+// 					duration.Nanoseconds())
+// 			},
+// 		),
+// 	}
+// 	// Hen we wantto customize the sampling of traceing to a probability of 50%
+// 	// not to miss import tracing
+// 	// halfSampler := trace.ProbabilitySampler(0.5)
+// 	// trace.ApplyConfig(trace.Config{
+// 	// 	DefaultSampler: func(p trace.SamplingParameters) trace.SamplingDecision {
+// 	// 		if strings.Contains(p.Name, "Produce") {
+// 	// 			return trace.SamplingDecision{Sample: true}
+// 	// 		}
+// 	// 		return halfSampler(p)
+// 	// 	},
+// 	// })
+// 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+// 	err := view.Register(ocgrpc.DefaultServerViews...)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func authenticate(ctx context.Context) (context.Context, error) {
-	peer, ok := peer.FromContext(ctx)
-	if !ok {
-		return ctx, status.New(
-			codes.Unknown,
-			"couldn't find peer info",
-		).Err()
-	}
+// 	opts = append(opts, grpc.StreamInterceptor(
+// 		grpc_middleware.ChainStreamServer(
+// 			grpc_auth.StreamServerInterceptor(authenticate),
+// 			grpc_zap.StreamServerInterceptor(logger, zapOpts...),
+// 		)), grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+// 		grpc_ctxtags.UnaryServerInterceptor(),
+// 		grpc_zap.UnaryServerInterceptor(logger, zapOpts...),
+// 		grpc_auth.UnaryServerInterceptor(authenticate),
+// 	)),
+// 		grpc.StatsHandler(&ocgrpc.ServerHandler{}),
+// 	)
 
-	if peer.AuthInfo == nil {
-		return context.WithValue(ctx, subjectContextKey{}, ""), nil
-	}
+// 	gsrv := grpc.NewServer(opts...)
+// 	srv, err := newgrpcServer(config)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	api.RegisterLogServer(gsrv, srv)
+// 	return gsrv, nil
+// }
 
-	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
-	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
-	ctx = context.WithValue(ctx, subjectContextKey{}, subject)
+// func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+// 	if err := s.Authorizer.Authorize(
+// 		subject(ctx),
+// 		objectWildcard,
+// 		produceAction,
+// 	); err != nil {
+// 		return nil, err
+// 	}
+// 	offset, err := s.CommitLog.Append(req.Record)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return ctx, nil
-}
+// 	return &api.ProduceResponse{Offset: offset}, nil
+// }
 
-func subject(ctx context.Context) string {
-	return ctx.Value(subjectContextKey{}).(string)
-}
+// func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+// 	// if err := s.Authorizer.Authorize(
+// 	// 	subject(ctx),
+// 	// 	objectWildcard,
+// 	// 	produceAction,
+// 	// ); err != nil {
+// 	// 	return nil, err
+// 	// }
+// 	record, err := s.CommitLog.Read(req.Offset)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &api.ConsumeResponse{Record: record}, nil
+// }
+
+// func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+// 	for {
+// 		req, err := stream.Recv()
+// 		if err != nil {
+// 			return err
+// 		}
+// 		res, err := s.Produce(stream.Context(), req)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		if err = stream.Send(res); err != nil {
+// 			return err
+// 		}
+// 	}
+// }
+
+// func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+// 	for {
+// 		select {
+// 		case <-stream.Context().Done():
+// 			return nil
+// 		default:
+// 			res, err := s.Consume(stream.Context(), req)
+// 			switch err.(type) {
+// 			case nil:
+// 			case api.ErrOffsetOutOfRange:
+// 				continue
+// 			default:
+// 				return err
+// 			}
+// 			if err = stream.Send(res); err != nil {
+// 				return err
+// 			}
+// 			req.Offset++
+// 		}
+// 	}
+// }
+
+// type subjectContextKey struct{}
+
+// func authenticate(ctx context.Context) (context.Context, error) {
+// 	peer, ok := peer.FromContext(ctx)
+// 	if !ok {
+// 		return ctx, status.New(
+// 			codes.Unknown,
+// 			"couldn't find peer info",
+// 		).Err()
+// 	}
+
+// 	if peer.AuthInfo == nil {
+// 		return context.WithValue(ctx, subjectContextKey{}, ""), nil
+// 	}
+
+// 	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
+// 	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
+// 	ctx = context.WithValue(ctx, subjectContextKey{}, subject)
+
+// 	return ctx, nil
+// }
+
+// func subject(ctx context.Context) string {
+// 	return ctx.Value(subjectContextKey{}).(string)
+// }
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,183 +2,84 @@ package server
 
 import (
 	"context"
-	"flag"
 	"io/ioutil"
 	"net"
-	"os"
 	"testing"
-	"time"
 
 	api "github.com/abdulmajid18/log-distributed-system/api/v1"
-	"github.com/abdulmajid18/log-distributed-system/internal/auth"
-	"github.com/abdulmajid18/log-distributed-system/internal/config"
 	"github.com/abdulmajid18/log-distributed-system/internal/log"
 	"github.com/stretchr/testify/require"
-	"go.opencensus.io/examples/exporter"
-	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
+	// "github.com/abdulmajid18/log-distributed-system/internal/auth"
+	// "github.com/abdulmajid18/log-distributed-system/internal/config"
+	// "go.opencensus.io/examples/exporter"
+	// "go.uber.org/zap"
+	// "google.golang.org/grpc"
+	// "google.golang.org/grpc/codes"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/status"
 	// 	"google.golang.org/grpc"
 	// 	"google.golang.org/grpc/codes"
 	// 	"google.golang.org/grpc/credentials"
 	// 	"google.golang.org/grpc/status"
 )
 
-var debug = flag.Bool("debug", false, "Enable observablity for debuging.")
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-	if *debug {
-		logger, err := zap.NewDevelopment()
-		if err != nil {
-			panic(err)
-		}
-		zap.ReplaceGlobals(logger)
-	}
-	os.Exit(m.Run())
-}
-
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
 		t *testing.T,
-		rootClient api.LogClient,
-		nobodyClient api.LogClient,
-		//config *Config
+		client api.LogClient,
+		config *Config,
 	){
-		"produce/consume a message to/from the log succeeds": testProduceConsume,
-		"produce/consume stream succeeds":                    testProduceConsumeStream,
-		"consume past log boundary fails":                    testConsumePastBoundary,
-		"unauthorized fails":                                 testUnauthorized,
+		"produce/consume a message to/from the log succeeeds": testProduceConsume,
+		"produce/consume stream succeeds":                     testProduceConsumeStream,
+		"consume past log boundary fails":                     testConsumePastBoundary,
 	} {
 		t.Run(scenario, func(t *testing.T) {
-			rootClient, nobodyClient, _, teardown := setupTest(t, nil)
+			client, config, teardown := setupTest(t, nil)
 			defer teardown()
-			fn(t, rootClient, nobodyClient)
+			fn(t, client, config)
 		})
 	}
 }
 
 func setupTest(t *testing.T, fn func(*Config)) (
-	rootClient api.LogClient,
-	nobodyClient api.LogClient,
+	client api.LogClient,
 	cfg *Config,
 	teardown func(),
 ) {
 	t.Helper()
-	l, err := net.Listen("tcp", "127.0.0.1:0")
+	l, err := net.Listen("tcp", ":0")
 	require.NoError(t, err)
-	// WithTransportCredentials and insecure.NewCredentials()
-	// clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-	// 	CertFile: config.ClientCertFile,
-	// 	KeyFile:  config.ClientKeyFile,
-	// 	CAFile:   config.CAFile,
-	// })
-	// require.NoError(t, err)
-
-	// clientCreds := credentials.NewTLS(clientTLSConfig)
-	// cc, err := grpc.Dial(
-	// 	l.Addr().String(),
-	// 	grpc.WithTransportCredentials(clientCreds),
-	// )
-	// require.NoError(t, err)
-
-	// client = api.NewLogClient(cc)
-	newClient := func(crtPath, keyPath string) (
-		*grpc.ClientConn,
-		api.LogClient,
-		[]grpc.DialOption,
-	) {
-		tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
-			CertFile: crtPath,
-			KeyFile:  keyPath,
-			CAFile:   config.CAFile,
-			Server:   false,
-		})
-		require.NoError(t, err)
-		tlsCreds := credentials.NewTLS(tlsConfig)
-		opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
-		conn, err := grpc.Dial(l.Addr().String(), opts...)
-		require.NoError(t, err)
-		client := api.NewLogClient(conn)
-		return conn, client, opts
-	}
-	var rootConn *grpc.ClientConn
-	rootConn, rootClient, _ = newClient(
-		config.RootClientCertFile,
-		config.RootClientKeyFile,
-	)
-	var nobodyConn *grpc.ClientConn
-	nobodyConn, nobodyClient, _ = newClient(
-		config.NobodyClientCertFile,
-		config.NobodyClientKeyFile,
-	)
-
-	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CertFile:      config.ServerCertFile,
-		KeyFile:       config.ServerKeyFile,
-		CAFile:        config.CAFile,
-		ServerAddress: l.Addr().String(),
-		Server:        true,
-	})
+	clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
 	require.NoError(t, err)
-	serverCreds := credentials.NewTLS(serverTLSConfig)
-
 	dir, err := ioutil.TempDir("/home/rozz/go/src/github.com/abdulmajid18/log-distributed-system/internal/server/", "server-test")
 	require.NoError(t, err)
-
 	clog, err := log.NewLog(dir, log.Config{})
 	require.NoError(t, err)
-
-	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
-
-	var telemetryExporter *exporter.LogExporter
-	if *debug {
-		metricsLogFile, err := ioutil.TempFile("", "metrics-*.log")
-		require.NoError(t, err)
-		t.Logf("metrics log file: %s", metricsLogFile.Name())
-		tracesLogFile, err := ioutil.TempFile("", "traces-*.log")
-		require.NoError(t, err)
-		t.Logf("traces log file: %s", tracesLogFile.Name())
-		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
-			MetricsLogFile:    metricsLogFile.Name(),
-			TracesLogFile:     tracesLogFile.Name(),
-			ReportingInterval: time.Second,
-		})
-		require.NoError(t, err)
-		err = telemetryExporter.Start()
-		require.NoError(t, err)
-	}
-
 	cfg = &Config{
-		CommitLog:  clog,
-		Authorizer: authorizer,
+		CommitLog: clog,
 	}
 	if fn != nil {
 		fn(cfg)
 	}
-	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
+	server, err := NewGRPCServer(cfg)
 	require.NoError(t, err)
-
 	go func() {
 		server.Serve(l)
 	}()
-
-	return rootClient, nobodyClient, cfg, func() {
+	client = api.NewLogClient(cc)
+	return client, cfg, func() {
 		server.Stop()
-		rootConn.Close()
-		nobodyConn.Close()
+		cc.Close()
 		l.Close()
-		if telemetryExporter != nil {
-			time.Sleep(1500 * time.Millisecond)
-			telemetryExporter.Stop()
-			telemetryExporter.Close()
-		}
+		// clog.Remove()
 	}
 }
 
-func testProduceConsume(t *testing.T, client api.LogClient, _ api.LogClient) {
+func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
 	ctx := context.Background()
 	want := &api.Record{
 		Value: []byte("hello world"),
@@ -198,7 +99,11 @@ func testProduceConsume(t *testing.T, client api.LogClient, _ api.LogClient) {
 	require.Equal(t, want.Offset, consume.Record.Offset)
 }
 
-func testConsumePastBoundary(t *testing.T, client api.LogClient, _ api.LogClient) {
+func testConsumePastBoundary(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
 	ctx := context.Background()
 	produce, err := client.Produce(ctx, &api.ProduceRequest{
 		Record: &api.Record{
@@ -212,7 +117,6 @@ func testConsumePastBoundary(t *testing.T, client api.LogClient, _ api.LogClient
 	if consume != nil {
 		t.Fatal("consume not nil")
 	}
-
 	got := status.Code(err)
 	want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
 	if got != want {
@@ -220,7 +124,11 @@ func testConsumePastBoundary(t *testing.T, client api.LogClient, _ api.LogClient
 	}
 }
 
-func testProduceConsumeStream(t *testing.T, client api.LogClient, _ api.LogClient) {
+func testProduceConsumeStream(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
 	ctx := context.Background()
 	records := []*api.Record{{
 		Value:  []byte("first message"),
@@ -265,33 +173,272 @@ func testProduceConsumeStream(t *testing.T, client api.LogClient, _ api.LogClien
 	}
 }
 
-func testUnauthorized(t *testing.T, _, client api.LogClient) {
-	ctx := context.Background()
+// var debug = flag.Bool("debug", false, "Enable observablity for debuging.")
 
-	produce, err := client.Produce(ctx, &api.ProduceRequest{
-		Record: &api.Record{
-			Value: []byte("New test"),
-		},
-	})
-	if produce != nil {
-		t.Fatalf("Produce offset should be nil")
-	}
+// func TestMain(m *testing.M) {
+// 	flag.Parse()
+// 	if *debug {
+// 		logger, err := zap.NewDevelopment()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		zap.ReplaceGlobals(logger)
+// 	}
+// 	os.Exit(m.Run())
+// }
 
-	got, want := status.Code(err), codes.PermissionDenied
+// func TestServer(t *testing.T) {
+// 	for scenario, fn := range map[string]func(
+// 		t *testing.T,
+// 		rootClient api.LogClient,
+// 		nobodyClient api.LogClient,
+// 		//config *Config
+// 	){
+// 		"produce/consume a message to/from the log succeeds": testProduceConsume,
+// 		"produce/consume stream succeeds":                    testProduceConsumeStream,
+// 		"consume past log boundary fails":                    testConsumePastBoundary,
+// 		"unauthorized fails":                                 testUnauthorized,
+// 	} {
+// 		t.Run(scenario, func(t *testing.T) {
+// 			rootClient, nobodyClient, _, teardown := setupTest(t, nil)
+// 			defer teardown()
+// 			fn(t, rootClient, nobodyClient)
+// 		})
+// 	}
+// }
 
-	if got != want {
-		t.Fatalf("Expected %s but got %s", want, got)
-	}
+// func setupTest(t *testing.T, fn func(*Config)) (
+// 	rootClient api.LogClient,
+// 	nobodyClient api.LogClient,
+// 	cfg *Config,
+// 	teardown func(),
+// ) {
+// 	t.Helper()
+// 	l, err := net.Listen("tcp", "127.0.0.1:0")
+// 	require.NoError(t, err)
+// 	// WithTransportCredentials and insecure.NewCredentials()
+// 	// clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+// 	// 	CertFile: config.ClientCertFile,
+// 	// 	KeyFile:  config.ClientKeyFile,
+// 	// 	CAFile:   config.CAFile,
+// 	// })
+// 	// require.NoError(t, err)
 
-	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: 0})
+// 	// clientCreds := credentials.NewTLS(clientTLSConfig)
+// 	// cc, err := grpc.Dial(
+// 	// 	l.Addr().String(),
+// 	// 	grpc.WithTransportCredentials(clientCreds),
+// 	// )
+// 	// require.NoError(t, err)
 
-	if consume != nil {
-		t.Fatalf("Consume offset should be nil")
-	}
+// 	// client = api.NewLogClient(cc)
+// 	newClient := func(crtPath, keyPath string) (
+// 		*grpc.ClientConn,
+// 		api.LogClient,
+// 		[]grpc.DialOption,
+// 	) {
+// 		tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
+// 			CertFile: crtPath,
+// 			KeyFile:  keyPath,
+// 			CAFile:   config.CAFile,
+// 			Server:   false,
+// 		})
+// 		require.NoError(t, err)
+// 		tlsCreds := credentials.NewTLS(tlsConfig)
+// 		opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+// 		conn, err := grpc.Dial(l.Addr().String(), opts...)
+// 		require.NoError(t, err)
+// 		client := api.NewLogClient(conn)
+// 		return conn, client, opts
+// 	}
+// 	var rootConn *grpc.ClientConn
+// 	rootConn, rootClient, _ = newClient(
+// 		config.RootClientCertFile,
+// 		config.RootClientKeyFile,
+// 	)
+// 	var nobodyConn *grpc.ClientConn
+// 	nobodyConn, nobodyClient, _ = newClient(
+// 		config.NobodyClientCertFile,
+// 		config.NobodyClientKeyFile,
+// 	)
 
-	got, want = status.Code(err), codes.PermissionDenied
+// 	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+// 		CertFile:      config.ServerCertFile,
+// 		KeyFile:       config.ServerKeyFile,
+// 		CAFile:        config.CAFile,
+// 		ServerAddress: l.Addr().String(),
+// 		Server:        true,
+// 	})
+// 	require.NoError(t, err)
+// 	serverCreds := credentials.NewTLS(serverTLSConfig)
 
-	if got != want {
-		t.Fatalf("Expected %s but got %s", want, got)
-	}
-}
+// 	dir, err := ioutil.TempDir("/home/rozz/go/src/github.com/abdulmajid18/log-distributed-system/internal/server/", "server-test")
+// 	require.NoError(t, err)
+
+// 	clog, err := log.NewLog(dir, log.Config{})
+// 	require.NoError(t, err)
+
+// 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+
+// 	var telemetryExporter *exporter.LogExporter
+// 	if *debug {
+// 		metricsLogFile, err := ioutil.TempFile("", "metrics-*.log")
+// 		require.NoError(t, err)
+// 		t.Logf("metrics log file: %s", metricsLogFile.Name())
+// 		tracesLogFile, err := ioutil.TempFile("", "traces-*.log")
+// 		require.NoError(t, err)
+// 		t.Logf("traces log file: %s", tracesLogFile.Name())
+// 		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+// 			MetricsLogFile:    metricsLogFile.Name(),
+// 			TracesLogFile:     tracesLogFile.Name(),
+// 			ReportingInterval: time.Second,
+// 		})
+// 		require.NoError(t, err)
+// 		err = telemetryExporter.Start()
+// 		require.NoError(t, err)
+// 	}
+
+// 	cfg = &Config{
+// 		CommitLog:  clog,
+// 		Authorizer: authorizer,
+// 	}
+// 	if fn != nil {
+// 		fn(cfg)
+// 	}
+// 	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
+// 	require.NoError(t, err)
+
+// 	go func() {
+// 		server.Serve(l)
+// 	}()
+
+// 	return rootClient, nobodyClient, cfg, func() {
+// 		server.Stop()
+// 		rootConn.Close()
+// 		nobodyConn.Close()
+// 		l.Close()
+// 		if telemetryExporter != nil {
+// 			time.Sleep(1500 * time.Millisecond)
+// 			telemetryExporter.Stop()
+// 			telemetryExporter.Close()
+// 		}
+// 	}
+// }
+
+// func testProduceConsume(t *testing.T, client api.LogClient, _ api.LogClient) {
+// 	ctx := context.Background()
+// 	want := &api.Record{
+// 		Value: []byte("hello world"),
+// 	}
+// 	produce, err := client.Produce(
+// 		ctx,
+// 		&api.ProduceRequest{
+// 			Record: want,
+// 		},
+// 	)
+// 	require.NoError(t, err)
+// 	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+// 		Offset: produce.Offset,
+// 	})
+// 	require.NoError(t, err)
+// 	require.Equal(t, want.Value, consume.Record.Value)
+// 	require.Equal(t, want.Offset, consume.Record.Offset)
+// }
+
+// func testConsumePastBoundary(t *testing.T, client api.LogClient, _ api.LogClient) {
+// 	ctx := context.Background()
+// 	produce, err := client.Produce(ctx, &api.ProduceRequest{
+// 		Record: &api.Record{
+// 			Value: []byte("hello world"),
+// 		},
+// 	})
+// 	require.NoError(t, err)
+// 	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+// 		Offset: produce.Offset + 1,
+// 	})
+// 	if consume != nil {
+// 		t.Fatal("consume not nil")
+// 	}
+
+// 	got := status.Code(err)
+// 	want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+// 	if got != want {
+// 		t.Fatalf("got err: %v, want: %v", got, want)
+// 	}
+// }
+
+// func testProduceConsumeStream(t *testing.T, client api.LogClient, _ api.LogClient) {
+// 	ctx := context.Background()
+// 	records := []*api.Record{{
+// 		Value:  []byte("first message"),
+// 		Offset: 0,
+// 	}, {
+// 		Value:  []byte("second message"),
+// 		Offset: 1,
+// 	}}
+// 	{
+// 		stream, err := client.ProduceStream(ctx)
+// 		require.NoError(t, err)
+// 		for offset, record := range records {
+// 			err = stream.Send(&api.ProduceRequest{
+// 				Record: record,
+// 			})
+// 			require.NoError(t, err)
+// 			res, err := stream.Recv()
+// 			require.NoError(t, err)
+// 			if res.Offset != uint64(offset) {
+// 				t.Fatalf(
+// 					"got offset: %d, want: %d",
+// 					res.Offset,
+// 					offset,
+// 				)
+// 			}
+// 		}
+// 	}
+// 	{
+// 		stream, err := client.ConsumeStream(
+// 			ctx,
+// 			&api.ConsumeRequest{Offset: 0},
+// 		)
+// 		require.NoError(t, err)
+// 		for i, record := range records {
+// 			res, err := stream.Recv()
+// 			require.NoError(t, err)
+// 			require.Equal(t, res.Record, &api.Record{
+// 				Value:  record.Value,
+// 				Offset: uint64(i),
+// 			})
+// 		}
+// 	}
+// }
+
+// func testUnauthorized(t *testing.T, _, client api.LogClient) {
+// 	ctx := context.Background()
+
+// 	produce, err := client.Produce(ctx, &api.ProduceRequest{
+// 		Record: &api.Record{
+// 			Value: []byte("New test"),
+// 		},
+// 	})
+// 	if produce != nil {
+// 		t.Fatalf("Produce offset should be nil")
+// 	}
+
+// 	got, want := status.Code(err), codes.PermissionDenied
+
+// 	if got != want {
+// 		t.Fatalf("Expected %s but got %s", want, got)
+// 	}
+
+// 	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: 0})
+
+// 	if consume != nil {
+// 		t.Fatalf("Consume offset should be nil")
+// 	}
+
+// 	got, want = status.Code(err), codes.PermissionDenied
+
+// 	if got != want {
+// 		t.Fatalf("Expected %s but got %s", want, got)
+// 	}
+// }
