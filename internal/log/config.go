383cf5752d302aec3fd8ffa678ba6a7740f9a76d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-07 14:20:12 +0000

    update: log

--- internal/log/config.go
+++ internal/log/config.go
@@ -1,16 +1,12 @@
 package log
 
-import (
-	"github.com/hashicorp/raft"
-)
-
 type Config struct {
-	Raft struct {
-		raft.Config
-		StreamLayer *StreamLayer
-		Bootstrap
-		bool
-	}
+	// Raft struct {
+	// 	raft.Config
+	// 	StreamLayer *StreamLayer
+	// 	Bootstrap
+	// 	bool
+	// }
 	Segment struct {
 		MaxStoreBytes uint64
 		MaxIndexBytes uint64
